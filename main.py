eecd0c4a9e43e6c2a4aa366007a9e5a0762b8508
hughwilliams <hhwilliamsnh@gmail.com>
2025-12-03 20:14:52 -0500

    excel

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+
+# We will be doing the rocket ship with 6 experiments
+
+#Dataframe
+file_path = '/Users/hughwilliams/Documents/Math/Operations_Research/Portfolio_Projects/Simulated_Annealing/rocket_ship.xlsx'
+df = pd.read_excel(file_path)
+
+
