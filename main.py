c0268364d25906c2d6959450b7e50b27ea1f5c27
hughwilliams <hhwilliamsnh@gmail.com>
2025-12-05 13:33:26 -0500

    WE DID IT

--- main.py
+++ main.py
@@ -6,32 +6,11 @@ import numpy as np
 
 # We will be doing the rocket ship with 6 experiments
 
-#Dataframe
-file_path = '/Users/hughwilliams/Documents/Math/Operations_Research/Portfolio_Projects/Simulated_Annealing/rocket_ship.xlsx'
-df = pd.read_excel(file_path)
-
-print(df)
-
-# initial soln will be random
-# objective function will be
-
-
-# initial soln
-x_init = [random.randint(0, 1) for _ in range(6)]
-
-# mu
-mu = 1
-
-
-
-
 def get_neighbor(x):
-    bit = random.randint(0,5)
-    if x[bit] == 1:
-        x[bit] = 0
-    else:
-        x[bit] = 1
-    return x
+    neighbor = x.copy()
+    bit = random.randint(0, 5)
+    neighbor[bit] = 1 - neighbor[bit]
+    return neighbor
 
 def penalty(x):
     rh = sum(df.loc[i, 'Weight']*x[i] for i in range(6)) - 220
@@ -39,7 +18,6 @@ def penalty(x):
     global mu
     pen = mu * (delta/220)
     return pen
-
 
 def get_obj_val(x):
     obj_val = sum(df.loc[i, 'Merit']*x[i] for i in range(len(df))) - penalty(x)
@@ -50,16 +28,60 @@ def accept(x,y,c):
     f_y = get_obj_val(y)
     f_x = get_obj_val(x)
     if f_y > f_x:
+        print("y was better than x, accepted y")
         return y
     else:
-        e = math.e
-        prob = e ** ((f_y - f_x)/c)
+        print("x was better than y")
+        prob = math.exp((f_y - f_x)/c)
+        prob = min(1, max(prob, 0))
+        print(prob)
+        print("\n")
         success = np.random.binomial(n=1, p=prob)
         if success == 1:
+            print("y was worse but we accepted y")
             return y
         else:
+           # print("y was worse and we rejected y")
             return x
-        
+
+def markov_chain(x,c,L_k):
+    current = x
+    for _ in range(L_k):
+        y = get_neighbor(current)
+        new_x = accept(current,y,c)
+        current = new_x
+    return current
 
 
-print(accept([0,0,0,1,1,0],[0,0,0,1,0,0],100))
+if __name__ == '__main__':
+
+    #Dataframe
+    file_path = '/Users/hughwilliams/Documents/Math/Operations_Research/Portfolio_Projects/Simulated_Annealing/rocket_ship.xlsx'
+    df = pd.read_excel(file_path)
+
+    # penalty parameter, mu
+    mu = 1000
+
+    # length of markov chain, L_k
+    L_k = 100
+
+    # initial temp, c0
+    c0 = 1000
+
+    # temperature decrement, alpha
+    alpha = 0.995
+
+    c = c0
+
+    # initial soln
+    x_init = [random.randint(0, 1) for _ in range(6)]
+
+    
+    while True:
+        if c <= c0 / L_k:
+            break
+        x = markov_chain(x_init,c,L_k)
+        x_init = x
+        c = alpha * c
+
+    print(f"We did it! {x_init} is our final answer. Yippee!")
